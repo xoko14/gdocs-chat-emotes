c7dde838ca4fa344333a2923c660bb21e1742147
xoko14 <xoquinperezb@gmail.com>
2022-01-13 19:44:31 +0100

    emote database in json

--- content.js
+++ content.js
@@ -1,3 +1,14 @@
+var url = chrome.runtime.getURL("db/emotelist.json")
+
+var emotelist
+
+fetch(url).then(data => {
+    data.json().then(json => {
+        emotelist = json
+        console.log(emotelist)
+    })
+})
+
 const targetNode = document;
 const observerOptions = {
     childList: true,
@@ -10,13 +21,17 @@ observer.observe(targetNode, observerOptions);
 
 function callback(mutationList, observer) {
     mutationList.forEach((mutation) => {
-        console.log(mutation.type)
+        //console.log(mutation.type)
         if (mutation.addedNodes.length) {
             mutation.addedNodes.forEach(element => {
                 //console.log(element.classList)
                 if (element.className == "docs-chat-message") {
                     var text = element.innerHTML
-                    text = text.replace("monkaS", "<img src=\"https://cdn.betterttv.net/emote/56e9f494fff3cc5c35e5287e/1x\"/>")
+
+                    emotelist.forEach(emote =>{
+                        text = text.replace(emote.name, `<img src=\"${emote.url}\" alt=\"${emote.name}\"/>`)
+                        console.log(emote)
+                    })
                     //console.log(text)
                     element.innerHTML = text
 
